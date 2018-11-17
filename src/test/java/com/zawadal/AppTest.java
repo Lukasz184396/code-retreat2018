3a33c948cc60200d7525e851db2c2bbf1f419cb0
Lukasz184396 <zawadalukasz88@gmail.com>
2018-11-17 14:28:32 +0100

    Added test for when cell is dead and has 3 neighbours

--- src/test/java/com/zawadal/AppTest.java
+++ src/test/java/com/zawadal/AppTest.java
@@ -23,10 +23,23 @@ public class AppTest
     }
 
     @Test
-    public void ifCellIsAliveAndHasMoreThanThreeNeighboursShouldBeAlive() {
+    public void ifCellIsAliveAndHasMoreThanThreeNeighboursShouldBeDead() {
         Cell cell = new Cell(new State(true), new NeighbourHood(4));
         Assert.assertEquals(new State(false), cell.getNextState());
     }
+    @Test
+    public void ifCellIsDeadAndHasMoreThanThreeNeighboursShouldBeDead() {
+        Cell cell = new Cell(new State(false), new NeighbourHood(4));
+        Assert.assertEquals(new State(false), cell.getNextState());
+    }
+    @Test
+    public void ifCellIsDeadAndHasMoreThreeNeighboursShouldBeAlive() {
+        Cell cell = new Cell(new State(false), new NeighbourHood(3));
+        Assert.assertEquals(new State(true), cell.getNextState());
+    }
+
+
+
 
 
 
